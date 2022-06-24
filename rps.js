7af597633255d6a048e4b1d89306c0e881aed45b
lukiakos <lukiakos@gmail.com>
2022-06-24 14:00:09 +0200

    Added UI functionality
    Added UI functionality, including buttons for rock, paper and scissors, as well as a NEW ROUND button for starting a new round.

--- rps.js
+++ rps.js
@@ -23,25 +23,67 @@ function playRound(playerSelection, computerSelection){
     } 
 }
 
-let win = 0
-let lose = 0
-let tie = 0
+let win = 0;
+let lose = 0;
+let tie = 0;
+let over = false;
 
-function game() {
-    for (let i = 0; i<5; i++) {
-        const playerSelection = window.prompt("Enter rock, paper or scissors").toLowerCase();
-        const computerSelection = computerPlay();
-        let currGame = playRound(playerSelection,computerSelection);
-        if (currGame == "win") {
+// const rockbtn = document.querySelector("#rock");
+// rockbtn.addEventListener("click", function(e) { 
+//    playRound(e.target.id, computerPlay())
+//})
+
+const bttns = document.querySelectorAll('button') //get all the buttons on the page and store them in the bttns nodelist
+bttns.forEach(bttn => bttn.addEventListener('click', function(e) { // add a click event listener to all of the buttons stored in the nodelist
+    if (!over && e.target.id !== 'reload') {
+        let result = playRound(e.target.id, computerPlay()) // play a round with the button the player pressed
+
+        const gridContainer = document.querySelector(".grid-container")
+        let resultP = document.createElement("span")
+        resultP.textContent = result.toUpperCase()
+        if (result == "win") {
             win += 1
-            console.log('You win!')
-        } else if (currGame == "lose") {
+            document.getElementById("playerCounter").innerHTML = win;
+            resultP.style.backgroundColor = "green";
+        } else if (result == "lose") {
             lose += 1
-            console.log('You lose!')
-        } else if (currGame == "tie") {
+            document.getElementById("computerCounter").innerHTML = lose;
+            resultP.style.backgroundColor = "red";
+        } else {
             tie += 1
-            console.log('You tied!')
+            document.getElementById("tieCounter").innerHTML = tie;
+            resultP.style.backgroundColor = "lightgray";
         }
+        resultP.style.fontSize = "x-large";
+        resultP.style.fontWeight = 1000;
+        gridContainer.prepend(resultP)
+        bttn.classList.add('buttonPressed')
+        let alertWin = document.createElement("div");
+        alertWin.style.fontSize = "xx-large";
+        alertWin.style.fontWeight = 1000;
+        if (win == 5) {
+            const resultsDiv = document.querySelector('#resultsDiv');
+            alertWin.textContent = "You've won!";
+            resultsDiv.prepend(alertWin);
+            over = true;
+        } else if (lose == 5) {
+            const resultsDiv = document.querySelector('#resultsDiv');
+            alertWin.textContent = "You've lost!";
+            resultsDiv.prepend(alertWin);
+            over = true;
+        }
+    } else if (e.target.id == "reload") {
+        location.reload()
+    } else {
+        alert ("Press 'NEW ROUND' if you want to play another round!")
     }
-    console.log(win,lose,tie)
+}))
+
+
+function removeTransition(e) {
+    if (e.propertyName !== "transform") return;
+    this.classList.remove("buttonPressed")
+
 }
+
+bttns.forEach(bttn => bttn.addEventListener('transitionend', removeTransition));
